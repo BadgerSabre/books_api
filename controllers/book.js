d0341299b095517b7f0a0d778c6625d737dce537
Brandon Dega <brandondega@gmail.com>
2022-06-06 20:20:22 -0500

    testing routes

--- controllers/book.js
+++ controllers/book.js
@@ -2,6 +2,44 @@ const router = require('express').Router()
 const Book = require('../models/book')
 
 
+router.get('/seed', (req, res) => {
+    Book.insertMany([{
+        "title": "The Shinobi Initiative",
+        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+        "year": 2014,
+        "quantity": 10,
+        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+      },
+      {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": 3,
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+      },
+      {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": 8,
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+      },
+      {
+        "title": "Wâˆ€RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": 4,
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+      }])
+        .then(res.status(200).json({
+            message: 'Seed successful'
+        }))
+        .catch(res.status(400).json({
+            message: 'Seed unsuccessful'
+        }))
+})
+
+
 // GET all books
 router.get('/', async (req,res) => {
     try {
@@ -28,8 +66,8 @@ router.get('/:id', async (req, res) => {
 // POST create new book
 router.post('/', async (req, res) => {
     try {
-        const book = await Book.create(req.body)
-        res.json(book)
+        await Book.create(req.body)
+        res.redirect('/books')
     } catch (error) {
         console.log(error)
         res.send('ERROR')
@@ -40,8 +78,8 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     try {
         const { id } = req.params
-        const book = await Book.findByIdAndUpdate(id, req.body)
-        res.json(book)
+        await Book.findByIdAndUpdate(id, req.body)
+        res.redirect('/books')
     } catch (error) {
         console.log(error)
         res.send('ERROR')
